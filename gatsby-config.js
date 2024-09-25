a7a0a107c429ec726cd32ab785adf694de0397eb
Noel <42847827+noel-johnson@users.noreply.github.com>
2024-09-24 20:15:08 -0400

    Finished Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `First Gatsby Project`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
